a7da1df74e094a815a5bb2ff2715bb75c3c5c6b6
Guilherme Augusto Vieira <guilhermeavdino@gmail.com>
2021-03-21 21:14:18 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,13 @@
 var layer1 = document.getElementById('layer1'); //Refefencia o elemento layer 1
+var layer2 = document.getElementById('layer2'); //Refefencia o elemento layer 1
+var text = document.getElementById('text'); //Refefencia o elemento layer text
 
 scroll = window.pageYOffset; //Recebe o valor vertical do scroll sempre que alterado
 
 document.addEventListener('scroll', function (e){ //Adiciona um evento a página, associando o evento do scroll
   scroll = window.pageYOffset; //Atribui o valor do scroll
   layer1.style.width = (100 + scroll/5) + '%';
-});
-
-//Realiza o mesmo procedimento com a layer 2
-var layer2 = document.getElementById('layer2'); //Refefencia o elemento layer 1
-
-scroll = window.pageYOffset; //Recebe o valor vertical do scroll sempre que alterado
-
-document.addEventListener('scroll', function (e){ //Adiciona um evento a página, associando o evento do scroll
-  scroll = window.pageYOffset; //Atribui o valor do scroll
   layer2.style.width = (100 + scroll/5) + '%';
   layer2.style.left = (scroll/50) + '%';
-});
-
-//Realiza o mesmo procedimento com o texto
-var text = document.getElementById('text'); //Refefencia o elemento layer 1
-console.log(text);
-
-scroll = window.pageYOffset; //Recebe o valor vertical do scroll sempre que alterado
-
-document.addEventListener('scroll', function (e){ //Adiciona um evento a página, associando o evento do scroll
-  scroll = window.pageYOffset; //Atribui o valor do scroll
   text.style.top = - (scroll/10) + '%';
 });
