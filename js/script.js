9ab099ca929e0c87ebddaec518f1392854bcecc7
Guilherme Augusto Vieira <guilhermeavdino@gmail.com>
2021-03-21 21:07:58 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@ var layer1 = document.getElementById('layer1'); //Refefencia o elemento layer 1
 scroll = window.pageYOffset; //Recebe o valor vertical do scroll sempre que alterado
 
 document.addEventListener('scroll', function (e){ //Adiciona um evento a página, associando o evento do scroll
-  var offset = window.pageYOffset; //Atualiza o valor de offset
-  scroll = offset; //Atribui o valor de offset no scroll
+  scroll = window.pageYOffset; //Atribui o valor do scroll
   layer1.style.width = (100 + scroll/5) + '%';
 });
 
@@ -14,8 +13,7 @@ var layer2 = document.getElementById('layer2'); //Refefencia o elemento layer 1
 scroll = window.pageYOffset; //Recebe o valor vertical do scroll sempre que alterado
 
 document.addEventListener('scroll', function (e){ //Adiciona um evento a página, associando o evento do scroll
-  var offset = window.pageYOffset; //Atualiza o valor de offset
-  scroll = offset; //Atribui o valor de offset no scroll
+  scroll = window.pageYOffset; //Atribui o valor do scroll
   layer2.style.width = (100 + scroll/5) + '%';
   layer2.style.left = (scroll/50) + '%';
 });
@@ -27,7 +25,6 @@ console.log(text);
 scroll = window.pageYOffset; //Recebe o valor vertical do scroll sempre que alterado
 
 document.addEventListener('scroll', function (e){ //Adiciona um evento a página, associando o evento do scroll
-  var offset = window.pageYOffset; //Atualiza o valor de offset
-  scroll = offset; //Atribui o valor de offset no scroll
+  scroll = window.pageYOffset; //Atribui o valor do scroll
   text.style.top = - (scroll/10) + '%';
 });
